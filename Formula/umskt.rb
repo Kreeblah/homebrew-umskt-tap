41d0f812ee96feda49efcf374e6027727f515b98
Kreeblah <kreeblah@gmail.com>
2025-07-13 19:58:45 -0700

    Update to 0.3.2

--- Formula/umskt.rb
+++ Formula/umskt.rb
@@ -1,35 +1,16 @@
 class Umskt < Formula
   desc "Windows and Office Activation Code Generator"
   homepage "https://github.com/UMSKT/UMSKT"
-  url "https://github.com/UMSKT/UMSKT.git",
-      tag:      "v0.2.0-beta",
-      revision: "27344f6d4b37316439cacc5eb496b3fbc6224f2d"
+  url "https://github.com/UMSKT/UMSKT/archive/refs/tags/v0.3.2-beta.tar.gz"
+  sha256 "9697861827c8874a40af6ec11aa32fffb7cfe0180117479f642f153b48fb4f0c"
   license "GPL-3.0-only"
   head "https://github.com/UMSKT/UMSKT.git", branch: "master"
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_BUILD_TYPE=Release", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" *std_cmake_args
     system "cmake", "--build", "build"
     bin.install "build/umskt"
-    lib.install "build/lib_umskt.dylib"
-    cp "build/_deps/fmt-build/libfmt.10.0.0.dylib", "#{prefix}/fmt.dylib"
-  end
-
-  def post_install
-    mv "#{prefix}/fmt.dylib", "#{lib}/libfmt.10.0.0.dylib"
-    ln "#{lib}/libfmt.10.0.0.dylib", "#{lib}/libfmt.10.dylib"
-    ln "#{lib}/libfmt.10.dylib", "#{lib}/libfmt.dylib"
-    system "install_name_tool", "-change", "@rpath/libfmt.10.dylib",
-      "#{lib}/libfmt.10.dylib", "#{lib}/lib_umskt.dylib"
-    system "install_name_tool", "-change", "@rpath/lib_umskt.dylib",
-      "#{lib}/lib_umskt.dylib", "#{bin}/umskt"
-    system "install_name_tool", "-change", "@rpath/libfmt.10.dylib",
-      "#{lib}/libfmt.10.dylib", "#{bin}/umskt"
-    system "codesign", "--force", "--deep", "--preserve-metadata=entitlements,requirements,flags,runtime",
-      "--sign", "-", "#{lib}/lib_umskt.dylib"
-    system "codesign", "--force", "--deep", "--preserve-metadata=entitlements,requirements,flags,runtime",
-      "--sign", "-", "#{bin}/umskt"
   end
 end
